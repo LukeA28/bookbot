a1c3c0cd9370458f2324ab2ba4b3ae7374f38673
LukeA28 <lmallen040@gmail.com>
2023-02-21 12:57:20 -0500

    defined function to order alphabetic characters by most to least frequent

--- main.py
+++ main.py
@@ -15,3 +15,23 @@ def letter_dictionary(book_string):
         else:
             letter_dict[low_letter] = 1
     return letter_dict
+
+def sort_results(dict):
+    origin_list = list(dict.items())
+    order_list = list()
+    highest = 0
+    while origin_list:
+        for pair in origin_list:
+            if pair[0].isalpha():
+                if pair[1] > highest:
+                    highest = pair[1]
+            else:
+                origin_list.remove(pair)
+        for couple in origin_list:
+            if couple[1] == highest:
+                order_list.append(couple)
+                origin_list.remove(couple)
+        highest = 0
+    return order_list
+
+print(sort_results(letter_dictionary(file_contents)))

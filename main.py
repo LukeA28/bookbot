d2e8704f8509a5116664347e59dd20bee8ef7c6e
LukeA28 <lmallen040@gmail.com>
2025-09-27 23:18:23 -0400

    change main.py to output number of words in frankenstein.txt

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
     
+def count_words(text_str):
+    words = text_str.split()
+    return len(words)
+    
 def main():
     file_text = get_book_text("books/frankenstein.txt")
-    print(file_text)
+    print(f"Found {count_words(file_text)} total words")
 
 main()

ba9fa721f0f0f701bfcb31dd1c1a277f6695a38e
LukeA28 <lmallen040@gmail.com>
2025-09-28 21:41:37 -0400

    completed statistic gathering functions, instructions for results display, changed text input source to CLI, added two more books

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from stats import count_words, count_chars
+from stats import count_words, count_chars, sort_on, sort_chars, print_chars
+import sys
+
+if len(sys.argv) > 1:
+    book_path = sys.argv[1]
+else:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
     
 def main():
-    file_text = get_book_text("books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+
+    file_text = get_book_text(book_path)
     word_count = count_words(file_text)
-    char_count = count_chars(file_text)
+    
     print(f"Found {word_count} total words")
-    print(char_count)
+    print("--------- Character Count -------")
+    
+    dictionary_list = sort_chars(count_chars(file_text))
+    
+    print_chars(dictionary_list)
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -5,11 +5,13 @@ def count_words(text_str):
 def count_chars(book_text):
     text_chars = {}
     lower_text = book_text.lower()
+
     for char in lower_text:
         if char in text_chars:
             text_chars[char] += 1
         else:
             text_chars[char] = 1
+            
     return text_chars
 
 def sort_on(items):
@@ -17,10 +19,15 @@ def sort_on(items):
 
 def sort_chars(char_dict):
     dict_list = []
-    char_pair = {}
-    for chrctr in char_dict:
-        char_pair["char"] = chrctr
-        char_pair["num"] = char_dict[chrctr]
+
+    for chrctr, num in char_dict.items():
+        char_pair = {"char": chrctr, "num": num}
         dict_list.append(char_pair)
-        char_pair.clear()
-    return dict_list.sort(reverse=True, key=sort_on)
+    
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
+
+def print_chars(char_list):
+    for dict in char_list:
+        if dict["char"].isalpha():
+            print(f"{dict['char']}: {dict['num']}")
